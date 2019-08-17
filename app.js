2478a920c5348852ff10e5a3f7172ebd034693e7
agw1010 <gunnwook02@gmail.com>
2019-08-17 16:13:26 +0900

    show error

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+const errorField = document.querySelector(".error-field");
+
 function animatedForm() {
   const arrows = document.querySelectorAll(".fa-arrow-down");
   arrows.forEach(arrow => {
@@ -25,8 +27,10 @@ function animatedForm() {
 function validateUser(user) {
   if (user.value.length < 6) {
     error("rgb(189,87,87");
+    errorField.innerText = `needs to have more than 6 letters`;
   } else {
     error("rgb(87, 189, 130)");
+    errorField.innerText = ``;
     return true;
   }
 }
@@ -35,9 +39,11 @@ function validateEmail(email) {
   const validation = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
   if (validation.test(email.value)) {
     error("rgb(87, 189, 130)");
+    errorField.innerText = ``;
     return true;
   } else {
     error("rgb(189,87,87");
+    errorField.innerText = `needs to be an email form`;
   }
 }
 
